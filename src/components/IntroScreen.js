f660005caf4fd36add55f02c337b8fd972e86a96
Samer Saber <samerelearning@outlook.com>
2025-06-30 15:41:17 +0200

    Add begin button

--- src/components/IntroScreen.js
+++ src/components/IntroScreen.js
@@ -11,38 +11,45 @@ const getGreeting = () => {
 
 const IntroScreen = ({ onFinish }) => {
   const [text, setText] = useState('');
+  const [started, setStarted] = useState(false);
   const [play] = useSound(typingSound, { volume: 0.5 });
 
   useEffect(() => {
-    const greetingMessage = getGreeting();
-    if (!greetingMessage) return;
+    if (!started) return;
 
+    const greetingMessage = getGreeting();
     let index = 0;
+
     const interval = setInterval(() => {
-        const char = greetingMessage[index];
-        if (char !== undefined) {
+      const char = greetingMessage[index];
+      if (char !== undefined) {
         setText((prev) => prev + char);
         play();
         index++;
-        } else {
+      } else {
         clearInterval(interval);
         setTimeout(() => onFinish(), 1000);
-        }
+      }
     }, 100);
 
     return () => clearInterval(interval);
-    }, [play, onFinish]);
-
-
+  }, [started, play, onFinish]);
 
   return (
-  <div className="typewriter-screen">
-    <h1>{text}<span className="cursor">█</span></h1>
-
-
-  </div>
-);
-
+    <div className="typewriter-screen flex-col">
+      {!started ? (
+        <button
+          className="text-[#00ff00] border border-[#00ff00] px-6 py-3 font-mono text-lg hover:bg-[#00ff00] hover:text-black transition-all"
+          onClick={() => setStarted(true)}
+        >
+          Begin Mission
+        </button>
+      ) : (
+        <h1>{text}<span className="cursor">█</span></h1>
+      )}
+    </div>
+  );
 };
 
 export default IntroScreen;
+
